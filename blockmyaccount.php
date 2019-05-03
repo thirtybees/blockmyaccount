a70449aafa168cab1f90ae9ee3e15de907e1e35b
Markus Hitter <mah@jump-ing.de>
2019-05-04 01:32:31 +0200

    Add constructor entry 'tb_min_version'.
    This is a requirement introduced recently by the validation script.

--- blockmyaccount.php
+++ blockmyaccount.php
@@ -41,6 +41,7 @@ class BlockMyAccount extends Module
 		$this->displayName = $this->l('Block My Account');
 		$this->description = $this->l('Displays a block with links relative to a user\'s account.');
 		$this->tb_versions_compliancy = '> 1.0.0';
+		$this->tb_min_version = '1.0.0';
 		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
 	}
 
